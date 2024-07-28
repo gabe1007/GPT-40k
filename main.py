73d81d5d0be7fa7896355b932871bb3f8e86df67
Gabriel <gabriel1007@yahoo.com>
2024-07-28 19:18:38 -0300

    refactor: Add MultiHeadAttention and Block classes for self-attention in Transformer

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ BLOCK_SIZE = 256 # what is the maximum context length for predictions?
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 VOCAB_SIZE = 1000
 N_EMBED = 512
+N_HEADS = 8
 DROPOUT = 0.2
+EVAL_ITERS = 200
+N_LAYER = 6
 
 def convert_to_tensor(text):
     """
@@ -59,6 +62,7 @@ def get_batch(split, block_size=BLOCK_SIZE, batch_size=BATCH_SIZE):
     x, y = x.to(DEVICE), y.to(DEVICE)
 
     return x, y
+    
 
 class Head(nn.Module):
     """ one head of self-attention """
@@ -66,8 +70,8 @@ class Head(nn.Module):
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(N_EMBED, head_size, bias=False) # head_size x N_EMBED
-        self.query = nn.Linear(N_EMBED, head_size, bias=False) # head_size x N_EMBED
-        self.value = nn.Linear(N_EMBED, head_size, bias=False) # head_size x N_EMBED
+        self.query = nn.Linear(N_EMBED, head_size, bias=False)
+        self.value = nn.Linear(N_EMBED, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)))
 
         self.dropout = nn.Dropout(DROPOUT)
@@ -89,7 +93,113 @@ class Head(nn.Module):
         v = self.value(x) # (B,T,hs)
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
+    
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
 
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, N_EMBED) # 512 x 512
+        self.dropout = nn.Dropout(DROPOUT)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+    
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(DROPOUT),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))      
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class GPTLanguageModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(VOCAB_SIZE, N_EMBED)
+        self.position_embedding_table = nn.Embedding(BLOCK_SIZE, N_EMBED)
+        self.blocks = nn.Sequential(*[Block(N_EMBED, n_head=N_HEADS) for _ in range(N_LAYER)])
+        self.ln_f = nn.LayerNorm(N_EMBED) # final layer norm
+        self.lm_head = nn.Linear(N_EMBED, VOCAB_SIZE) # language model head
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=DEVICE)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
 
 if __name__ == '__main__':
     with open('./Know_no_fear.txt', 'r', encoding='utf-8') as f:
@@ -108,9 +218,8 @@ if __name__ == '__main__':
 
     new_emb = emb + emb_pos # B,T,C + T,C
 
-    head = Head(64)
-    head.forward(new_emb) # B,T,C
-    
+    foo = Block(N_EMBED, N_HEADS)
+    foo.forward(new_emb)
     
     
 
